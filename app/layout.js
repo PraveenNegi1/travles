2bd92b2ff21822af53cb7318a6f24d499e83c0dc
PraveenNegi1 <praveennegi700@gmail.com>
2025-07-24 13:19:12 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -40,7 +40,13 @@ export const metadata = {
     shortcut: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
-  viewport: "width=device-width, initial-scale=1",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({ children }) {
