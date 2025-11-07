5f8787864759166edc5c6b352ede8bd506345b0b
PraveenNegi1 <praveennegi700@gmail.com>
2025-11-07 10:59:09 +0530

    done

--- app/(frontend)/district/nainital/page.jsx
+++ app/(frontend)/district/nainital/page.jsx
@@ -99,13 +99,13 @@ const Page = () => {
 
   return (
     <div className="p-8 bg-gray-50">
-      <div className="flex justify-center mb-8 w-full">
+      <div className="flex justify-center mb-8 w-full h-[50vh] sm:h-[80vh] relative">
         <Image
           src="https://images.unsplash.com/photo-1610712147665-04400af97a32?q=80&w=1974&auto=format&fit=crop"
           alt="Nainital"
           width={1000}
           height={1000}
-          className="rounded-xl shadow-lg"
+          className="rounded-xl shadow-lg object-cover brightness-75 w-full h-full"
         />
       </div>
 
