46d569e623e2d7fd45b835cd23c7cc759d001697
PraveenNegi1 <praveennegi700@gmail.com>
2025-11-13 14:46:59 +0530

    done

--- app/(frontend)/district/chamoli/page.jsx
+++ app/(frontend)/district/chamoli/page.jsx
@@ -107,7 +107,7 @@ const Page = () => {
           alt="Chamoli"
           width={1000}
           height={1000}
-          className="rounded-xl shadow-lg object-cover brightness-75 w-full h-full"
+          className="rounded-xl shadow-lg object-cover brightness-80 w-full h-full"
         />
       </div>
       <div className="text-center mb-8 merriweather">
