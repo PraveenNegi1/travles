78410c1f6c2a44e3db81bee5528f4e845818749a
PraveenNegi1 <praveennegi700@gmail.com>
2026-02-06 16:31:17 +0530

    done

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -160,9 +160,9 @@ export default function LoginPage() {
         </form>
 
         <div className="flex items-center my-6">
-          <div className="flex-grow border-t border-gray-300"></div>
+          <div className="grow border-t border-gray-300"></div>
           <span className="px-2 text-sm text-gray-500">or</span>
-          <div className="flex-grow border-t border-gray-300"></div>
+          <div className="grow border-t border-gray-300"></div>
         </div>
 
         <button
